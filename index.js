02cb5edd87b66716a9b52aec155d48ef7f7c642c
Victor Kemboi <vickemtoo97@gmail.com>
2024-03-22 22:43:52 +0300

    looping challange

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (arrName, arrGift) => {
+    let  giftCard = []
+    for (let i=0; i< arrName.length; i++){
+        giftCard.push(`Thank you, ${arrName[i]}, for the wonderful ${arrGift} gift!`)
+}
+return giftCard;
+}
+const countDown =( generateNumber ) => {
+    while ( generateNumber > 0 ) {
+      console.log( generateNumber );
+      generateNumber -= 1;
+    }
+    console.log( generateNumber );
+  }
